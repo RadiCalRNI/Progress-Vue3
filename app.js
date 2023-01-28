89803e580b9f684a9f77b2689ca1e86d5a85c0a9
Mohammad Rouhani <m.rouhani2001@gmail.com>
2023-01-28 13:11:40 +0330

    progress bar colors created with js and html

--- app.js
+++ app.js
@@ -2,17 +2,36 @@ Vue.createApp({
   data() {
     return {
       progress: 0,
+      isFirst: true,
+      isCenter: false,
+      isLast: false,
     };
   },
 
   methods: {
     increase() {
       if (this.progress == 100) return;
+
+      if (this.progress > 30 && this.progress < 70) {
+        this.isFirst = false;
+        this.isCenter = true;
+      } else if (this.progress >= 70) {
+        this.isCenter = false;
+        this.isLast = true;
+      }
+
       this.progress += 1;
     },
 
     decrease() {
       if (this.progress == 0) return;
+      if (this.progress > 30 && this.progress < 70) {
+        this.isCenter = true;
+        this.isLast = false;
+      } else if (this.progress <= 30) {
+        this.isFirst = true;
+        this.isCenter = false;
+      }
       this.progress -= 1;
     },
   },
